8ab8e38ce65df3b9e7146db56dbfea01494fa81d
wobominhduc <nguyenminhduc.31k10@gmail.com>
2017-07-04 20:31:36 +0700

    Vi du function

--- d.js
+++ d.js
@@ -9,19 +9,30 @@
 // }
 //  Khai bao lop doi tuong
 // function or class
-function Employee(ten, tuoi){
-    this.name = ten;
-    this.age = tuoi;
+function Car(branch, year){
+    this.branch = branch;
+    this.year = year;
+}
+function Employee(name, age, car){
+    this.name = name;
+    this.age = age;
+    this.car = car;
     this.sayHello = function(){    // function namefunction(){}
-        console.log("Say Hello");
+        console.log("Xin chao toi la : " + this.name + ' | Tuoi: ' + this.age);
+    }
+    this.incrAge = function(){
+        this.age++;
     }
 }
-
+const y = new Car('Honda',2017);
 // console.log(khoa);
 // console.log(khoa.name + ": " + khoa.age);
 // console.log(khoa.bike.branch);
-const khoa = new Employee("Khoa Pham",30);
-const pham = new Employee("Hong Minh", 40);
-console.log(khoa);
-console.log(pham.name);
-khoa.sayHello();
+const khoa = new Employee("Khoa Pham",30, y);
+// const pham = new Employee("Hong Minh", 40);
+// console.log(khoa);
+// console.log(pham.name);
+// khoa.sayHello();
+// khoa.incrAge();
+// khoa.sayHello();
+console.log(khoa.car.branch);
